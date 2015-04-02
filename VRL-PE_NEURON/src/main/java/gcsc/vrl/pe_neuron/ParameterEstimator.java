92cf0d50cec28e62436a9c9e3bbc057179f9b48e
stephanmg <stephan@syntaktischer-zucker.de>
2015-04-02 17:09:48 +0200

    Dependency fix.

--- VRL-PE_NEURON/src/main/java/gcsc/vrl/pe_neuron/ParameterEstimator.java
+++ VRL-PE_NEURON/src/main/java/gcsc/vrl/pe_neuron/ParameterEstimator.java
@@ -6,51 +6,64 @@ import eu.mihosoft.vrl.system.VSysUtil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
 import param_est.newton;
-import param_est.param_est;
 
 /**
- * This class implements the parameter estimator written by Ivo Muha, Friedrich Sch<&auml>ufele and Amine Taktak
+ * This class implements the parameter estimator written by Ivo Muha, Friedrich
+ * Sch<&auml>ufele and Amine Taktak
+ *
  * @author myra
  */
 @ComponentInfo(name = "Parameter Estimator", category = "Optimization/NEURON", description = "")
-public class ParameterEstimator implements Serializable{
-    
-    public void runParameterEstimator(String path,
-            ModelManipulation modeldata,
-            ExpDataManipulation expdata,
-            MethodOptions options,
-            PE_Options param_properties)throws IOException, ParserConfigurationException{
-        
+public class ParameterEstimator implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public void runParameterEstimator(String path,
+		ModelManipulation modeldata,
+		ExpDataManipulation expdata,
+		MethodOptions options,
+		PE_Options param_properties) throws IOException, ParserConfigurationException {
+
         //TODO: 
-        //1. create the luascript required by the parameter estimator
-        FileCreator fc = new FileCreator();
-        fc.createFile(modeldata, expdata, path, options, param_properties);
-        
-        //2. rufe den Parameterschaetzer mittels shellscript auf! Dazu brauchen wir vorraussichtlich VSystUtil und eventuell noch andere Klassen der VRL
-        if(VSysUtil.isMacOSX()){
-            
-            ByteArrayClassLoader classloader = new ByteArrayClassLoader(); 
-            InputStream in = classloader.getResourceAsStream("ugshell");
-            
+		//1. create the luascript required by the parameter estimator
+		FileCreator fc = new FileCreator();
+		fc.createFile(modeldata, expdata, path, options, param_properties);
+
+		//2. rufe den Parameterschaetzer mittels shellscript auf! Dazu brauchen wir vorraussichtlich VSystUtil und eventuell noch andere Klassen der VRL
+		if (VSysUtil.isMacOSX()) {
+
+			ByteArrayClassLoader classloader = new ByteArrayClassLoader();
+			InputStream in = classloader.getResourceAsStream("ugshell");
+
 //            param_est estimator = new param_est();
-            
-            newton n = new newton();
-            n.load_from_xml(path+"paramEst.xml");
-            n.perform_fit();
-            System.exit(0);
-            
-        }
-        
-        if(VSysUtil.isWindows()){
-            System.out.println(">> Not yet supported!");
-        }
-        
-        if(VSysUtil.isLinux()){
-            System.out.println(">> Not yet supported!");
-        }
-        
-    }
-    
+			newton n = new newton();
+			n.load_from_xml(path + "paramEst.xml");
+			try {
+				try {
+					n.perform_fit();
+				} catch (InvocationTargetException ex) {
+					Logger.getLogger(ParameterEstimator.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			} catch (InterruptedException iie) {
+				System.err.println(iie);
+				System.exit(0);
+			}
+
+		}
+
+		if (VSysUtil.isWindows()) {
+			System.out.println(">> Not yet supported!");
+		}
+
+		if (VSysUtil.isLinux()) {
+			System.out.println(">> Not yet supported!");
+		}
+
+	}
+
 }
