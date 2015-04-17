566b9cd83329ef5dfaa02cdf478003865fbcaa88
Myra Huymayer <m.huymayer@yahoo.de>
2015-04-17 17:59:21 +0200

    copy binary upon plugin installation

--- VRL-PE_NEURON/src/main/java/gcsc/vrl/pe_neuron/PENPluginConfigurator.java
+++ VRL-PE_NEURON/src/main/java/gcsc/vrl/pe_neuron/PENPluginConfigurator.java
@@ -1,15 +1,26 @@
 package gcsc.vrl.pe_neuron;
 
+import eu.mihosoft.vrl.io.IOUtil;
 import eu.mihosoft.vrl.system.InitPluginAPI;
 import eu.mihosoft.vrl.system.PluginAPI;
 import eu.mihosoft.vrl.system.PluginIdentifier;
 import eu.mihosoft.vrl.system.VPluginAPI;
 import eu.mihosoft.vrl.system.VPluginConfigurator;
+import eu.mihosoft.vrl.system.VRLPlugin;
+import eu.mihosoft.vrl.system.VSysUtil;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author myra
  */
 public class PENPluginConfigurator extends VPluginConfigurator{
+    
+    private File binaryCopy; 
     
     public PENPluginConfigurator(){
         //specification of plugin name and version
@@ -52,9 +63,29 @@ public class PENPluginConfigurator extends VPluginConfigurator{
 
     @Override
     public void install(InitPluginAPI iApi) {
-       iApi.getResourceFolder();
+        binaryCopy = new File(iApi.getResourceFolder(), "ugshell"); 
+        saveBinary();
+        setExecutable(binaryCopy);
     }
     
+    private void saveBinary(){
+        //irgendwas stimmt mit dieser Methode noch nicht! Das muss ich nochmal testen! 
+        InputStream is = getClass().getClassLoader().getResourceAsStream(
+                VSysUtil.getSystemBinaryPath()+"ugshell");
+        
+        try{
+            IOUtil.saveStreamToFile(is, binaryCopy);
+        }catch(FileNotFoundException ex){
+            Logger.getLogger(VRLPlugin.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(IOException ex){
+            Logger.getLogger(VRLPlugin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
+    private void setExecutable(File file){
+        if(!file.canExecute()){
+            file.setExecutable(true);
+        }
+    }
 
 }
